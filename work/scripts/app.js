01d2613993fcab027ef8e6d8bbfab4b4c0833701
Juuso Vuorinen <Juuso Vuorinen>
2017-05-04 11:56:08 +0300

    implemented firebase hosting

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// File modified to show more cities
+
 
 (function() {
   'use strict';
